c238e4cd8bf5b34ae18034eb6ec02b6396286198
Santrupt Potphode <santrupt.potphode29@gmail.com>
2026-03-29 00:10:43 +0530

    Actuator implemented + some minor changes

--- postgres-init/init.sql
+++ postgres-init/init.sql
@@ -1,4 +1,17 @@
+-- CREATE USER admin_user WITH PASSWORD 'password';
+-- CREATE DATABASE scribble;
+-- GRANT ALL PRIVILEGES ON DATABASE scribble TO admin_user;
+-- ALTER DATABASE scribble OWNER TO admin_user;
+
+-- Create your custom user (if you use this in your other services)
 CREATE USER admin_user WITH PASSWORD 'password';
-CREATE DATABASE scribble;
-GRANT ALL PRIVILEGES ON DATABASE scribble TO admin_user;
-ALTER DATABASE scribble OWNER TO admin_user;
+
+-- 1. Create and configure the User Service database
+CREATE DATABASE scribble_users;
+GRANT ALL PRIVILEGES ON DATABASE scribble_users TO admin_user;
+ALTER DATABASE scribble_users OWNER TO admin_user;
+
+-- 2. Create and configure the Word Service database
+CREATE DATABASE scribble_words;
+GRANT ALL PRIVILEGES ON DATABASE scribble_words TO admin_user;
+ALTER DATABASE scribble_words OWNER TO admin_user;
